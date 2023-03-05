88fcee91f399532498282b6d2d0b50b7185c5b80
Nejra Rizvić <nejra.rizvic.20@size.ba>
2023-03-05 23:30:46 +0100

    push -- all origin

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,18 @@
 
 
 export default function Main(){
-   return <p>main</p>;
+   return (
+   <div className="app-main">
+      <div className="app-main-note-edit">
+         <input type="text" id="title" autoFocus></input>
+     <textarea id="body" placeholder="Write your note here . . ."></textarea>
+      </div>
+
+      <div className="app-mainnote-preview">
+         <h1 className="preview-title">TITLE</h1>
+         <div className="markdown-preview">note preview</div>
+      </div>
+
+   </div>
+      )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,7 +3,7 @@
 export default function Sidebar(){
    return (
    <div className="app-sidebar">
-      <div className="app-sidebar-headre">
+      <div className="app-sidebar-header">
          <h1>Notes</h1>
          <button>Add</button>
       </div>
